849185c1fe598e1e9e6af73745568f1775de39ec
Faliture <59296445+Faliture@users.noreply.github.com>
2021-02-07 19:30:46 +0300

    Add files via upload

---
+++ virustech/virustech.cpp
@@ -0,0 +1,12 @@
+﻿#include <Windows.h>
+
+int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR nCmdLine, int nCmdShow) {
+	HDC hDc = GetWindowDC(GetDesktopWindow());
+	int x = rand() % GetSystemMetrics(SM_CXSCREEN);
+	int y = rand() % GetSystemMetrics(SM_CYSCREEN);
+	while (true) {
+		x = rand() % GetSystemMetrics(SM_CXSCREEN);
+		y = rand() % GetSystemMetrics(SM_CYSCREEN);
+		DrawIcon(hDc, x, y, LoadIcon(0, IDI_ERROR));
+	}
+}
